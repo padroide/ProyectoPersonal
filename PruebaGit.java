1529f632df97d0871dc2e5ead8b4b0b2dd4f3247
padroide <padroide@gmail.com>
2017-06-27 14:46:07 -0600

    Add files via upload

---
+++ PruebaGit.java
@@ -0,0 +1,16 @@
+package pruebagit;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Pablo
+ */
+public class PruebaGit {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = JOptionPane.showInputDialog("Ingrese su Nombre");
+        JOptionPane.showMessageDialog(null,"Tu Nombre es: "+nombre);
+    }
+}
